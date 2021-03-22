259789ae7f82ba1807538472c58e3a46b419191c
salavat876 <mavlikhanov2002@mail.ru>
2021-03-23 01:49:56 +0400

    create new cfg for my "lovely"webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import avg from './some.js';
+import './button.css';
 
-$('.title').html('текст');
+$('.title').html('текст3');
 console.log(avg(1,2,3));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let conf ={
-    entry:'./src/index.js',
-    output:{
-        path:path.resolve(__dirname,'./dist'),
-        filename:'main.js',
-        publicPath:'dist/'
-    }
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/'),
+        publicPath: '/dist/',
+        hot: true,
+        port: 8080,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: '/index.html',
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
 }
 
 module.exports = conf;
